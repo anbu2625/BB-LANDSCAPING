46c56c74b370d2967ff60b1a37b79fe350a51ecf
anbu2625 <your_github_email@example.com>
2026-07-23 20:47:07 +0530

    Updated CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -7,23 +7,41 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect MongoDB
 connectDB();
 
-app.use(cors());
+// CORS Configuration
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://bb-landscaping-git-main-aj-team3.vercel.app",
+      "https://bb-landscaping-chl8kwe9i-aj-team3.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+// Middleware
 app.use(express.json());
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("BB Landscaping API Running");
 });
 
+// Routes
 app.use(
   "/api/auth",
   require("./routes/authRoutes")
 );
+
 app.use(
   "/api/enquiries",
   require("./routes/enquiryRoutes")
 );
+
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
